cc0dc249ea157fc736f7fd2cd758226ef310cfc4
tammytdo11 <tammytdo11@gmail.com>
2019-07-09 15:26:02 -0700

    array shift code challenge

---
+++ code-challenges/array_shift/array_shift.py
@@ -0,0 +1,12 @@
+import math
+
+list1 = [1,2,3,4,5]
+val1 = 16
+
+def insert_shift_array(lst, value):
+    length = len(lst)
+    half = length / 2
+    half = int(math.ceil(length / 2))
+    return lst[:half] + [value] + lst[half:]
+
+print(insert_shift_array(list1, val1))
