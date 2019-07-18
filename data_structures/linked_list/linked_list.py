0d6fabb1e09748dc057ee53e007d73de5702f6a8
tammytdo11 <tammytdo11@gmail.com>
2019-07-18 00:29:55 -0700

    merge_LL.jpg added

--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -71,7 +71,76 @@ class LinkedList():
         if current.value == key:
             new_node.next = current.next
             current.next = new_node
-    
+
+    def kth_from_end(self, k, value):
+        new_node = Node(value)
+        current = self.head
+
+        if current:
+            while current.next != None:
+                if current.next == None:
+                    k = current.value
+                    return current.value
+                elif current.next != None:
+                    current = current.next
+            return ValueError
+                
+            
+    def kth_from_end(self, k):
+        
+
+         basic_counter = 0	        basic_counter = 0
+        length = 1	        length = 0
+        current = self.head	        current = self.head
+
+
+         while current.next:	        while current.next:
+            length += 1	            length += 1
+            current = current.next	            current = current.next
+
+
+         k_endpoint = (length - k)	        k_endpoint = ((length + 1) - k)
+
+
+         while basic_counter != k_endpoint:	        while basic_counter != k_endpoint:
+            current = self.head	            current = self.head
+            basic_counter += 1	            basic_counter += 1
+            current = current.next	            current = current.next
+
+
+         return current.next.node_value	        if k >= 1:
+            return current.next.node_value
+        elif k < 1:
+            return self.head.node_value
+
+
+def ll_merge(self, list2):
+       head1 = self.head
+       head2 = self.head
+       if not head1 and not head2:
+           return 'empty linked list'
+       if head1 and not head2:
+           return head1
+       if not head1 and head2:
+           return head2
+       if head1 and head2:
+           curr1 = head1
+           curr2 = head2
+           while curr1._next is not None and curr2._next is not None:
+               ref1 = curr1._next
+               ref2 = curr2._next
+               curr1._next = curr2
+               curr2._next = ref1
+               curr1 = ref1
+               curr2 = ref2
+           if curr1._next is None and curr2:
+               curr1._next = curr2
+               return head1
+           if curr2._next is None and curr1:
+               ref1 = curr1._next
+               curr1.next = curr2
+               curr2.next = ref1
+               return head1
 
 class Node():
     def __init__(self, value, next=None):
