872ddf67e8f77a3d988f23acf35b6c366f46e4bd
tammytdo11 <tammytdo11@gmail.com>
2019-07-28 17:43:32 -0700

    merge_lists code challenge

--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -84,63 +84,21 @@ class LinkedList():
                 elif current.next != None:
                     current = current.next
             return ValueError
-                
-            
-    def kth_from_end(self, k):
-        
 
-         basic_counter = 0	        basic_counter = 0
-        length = 1	        length = 0
-        current = self.head	        current = self.head
+    def merge_lists(self, list_a, list_b):
+        current = list_a.head
+        a_curr = list_a.head
+        b_curr = list_b.head
 
+        while a_curr and b_curr:
+            current.next = b_curr
+            b_curr = b_curr.next
+            current = current.next
+            current.next = a_curr
+            current = current.next
 
-         while current.next:	        while current.next:
-            length += 1	            length += 1
-            current = current.next	            current = current.next
+        return current
 
-
-         k_endpoint = (length - k)	        k_endpoint = ((length + 1) - k)
-
-
-         while basic_counter != k_endpoint:	        while basic_counter != k_endpoint:
-            current = self.head	            current = self.head
-            basic_counter += 1	            basic_counter += 1
-            current = current.next	            current = current.next
-
-
-         return current.next.node_value	        if k >= 1:
-            return current.next.node_value
-        elif k < 1:
-            return self.head.node_value
-
-
-def ll_merge(self, list2):
-       head1 = self.head
-       head2 = self.head
-       if not head1 and not head2:
-           return 'empty linked list'
-       if head1 and not head2:
-           return head1
-       if not head1 and head2:
-           return head2
-       if head1 and head2:
-           curr1 = head1
-           curr2 = head2
-           while curr1._next is not None and curr2._next is not None:
-               ref1 = curr1._next
-               ref2 = curr2._next
-               curr1._next = curr2
-               curr2._next = ref1
-               curr1 = ref1
-               curr2 = ref2
-           if curr1._next is None and curr2:
-               curr1._next = curr2
-               return head1
-           if curr2._next is None and curr1:
-               ref1 = curr1._next
-               curr1.next = curr2
-               curr2.next = ref1
-               return head1
 
 class Node():
     def __init__(self, value, next=None):
--- data_structures/linked_list/test_linked_list.py
+++ data_structures/linked_list/test_linked_list.py
@@ -56,3 +56,34 @@ def test_insert_after():
     ll.insert_after('indigo', 'green')
     assert ll.head.next.next.value == 'green'
 
+def test_merge_lists_exists():
+    assert LinkedList.merge_lists
+
+def test_merge_lists_even_lists():
+    list_a = LinkedList()
+    list_b = LinkedList()
+
+    list_a.insert('3')
+    list_a.insert('2')
+    list_a.insert('1')
+    list_b.insert('c')
+    list_b.insert('b')
+    list_b.insert('a')
+    list_a.merge_lists(list_a, list_b)
+
+    assert list_a.head.value == '1'
+
+def test_merge_lists_uneven_lists():
+    list_a = LinkedList()
+    list_b = LinkedList()
+
+    list_a.insert('4')
+    list_a.insert('3')
+    list_a.insert('2')
+    list_a.insert('1')
+    list_b.insert('b')
+    list_b.insert('a')
+    list_a.merge_lists(list_a, list_b)
+
+    assert list_a.head.value == '1'
+    assert list_a.head.next.value == 'b'
