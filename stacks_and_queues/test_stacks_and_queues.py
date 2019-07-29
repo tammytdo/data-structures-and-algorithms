17d6366a5bf0a9ec23b89d090a2b079ed46c5893
tammytdo11 <tammytdo11@gmail.com>
2019-07-28 20:00:41 -0700

    added stack and queue classes

---
+++ stacks_and_queues/test_stacks_and_queues.py
@@ -0,0 +1,33 @@
+from stacks_and_queues import LinkedList, Node, Stack, Queue
+
+
+def test_stack_exists():
+    assert == Stack()
+
+# def test_():
+#     assert ==
+
+# def test_():
+#     assert ==
+
+# def test_():
+#     assert ==
+
+# def test_():
+#     assert ==
+
+# def test_():
+#     assert ==
+
+# Can successfully push onto a stack
+# Can successfully push multiple values onto a stack
+# Can successfully pop off the stack
+# Can successfully empty a stack after multiple pops
+# Can successfully peek the next item on the stack
+# Can successfully instantiate an empty stack
+# Can successfully enqueue into a queue
+# Can successfully enqueue multiple values into a queue
+# Can successfully dequeue out of a queue the expected value
+# Can successfully peek into a queue, seeing the expected value
+# Can successfully empty a queue after multiple dequeues
+# Can successfully instantiate an empty queue
