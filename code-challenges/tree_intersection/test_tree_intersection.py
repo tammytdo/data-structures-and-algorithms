735d8985697e17d27df9030c3af0d071bf0dbd8f
tammytdo11 <tammytdo11@gmail.com>
2019-08-30 11:42:35 -0700

    testing fixture for tree1 and tree2 created

--- code-challenges/tree_intersection/test_tree_intersection.py
+++ code-challenges/tree_intersection/test_tree_intersection.py
@@ -29,5 +29,34 @@ def tree1():
 
     return BinaryTree(fifteen)
 
+
+@pytest.fixture():
+def tree2():
+    four = Node(4)
+    ten = Node(10)
+    one = Node(1)
+    sixteen = Node(16)
+    twelve = Node(12)
+    seventeen = Node(17)
+    sixty = Node(60)
+    twenty = Node(20)
+    thirty_five = Node(35)
+    four = Node(4)
+    fifty = Node(50)
+
+    four.left = ten
+    ten.left = fifteen
+    ten.right = sixteen
+    sixteen.left = twelve
+    sixteen.right = seventeen
+    four.right = sixty
+    sixty.left = twenty
+    sixty.right = thirty_five
+    thirty_five.left = four
+    thirty_five.right = fifty
+
+    return BinaryTree(four)
+
+
 # def test_tree_intersection(tree1, tree2):
 #     assert 
