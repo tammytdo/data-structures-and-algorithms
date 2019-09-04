0857780e362af91a2f51e503ec430663e0c133b5
tammytdo11 <tammytdo11@gmail.com>
2019-09-03 19:55:57 -0700

    all tests passing

--- code-challenges/tree_intersection/test_tree_intersection.py
+++ code-challenges/tree_intersection/test_tree_intersection.py
@@ -41,7 +41,6 @@ def tree2():
     sixty = Node(60)
     twenty = Node(20)
     thirty_five = Node(35)
-    four = Node(4)
     fifty = Node(50)
 
     four.left = ten
@@ -52,10 +51,24 @@ def tree2():
     four.right = sixty
     sixty.left = twenty
     sixty.right = thirty_five
-    thirty_five.left = four
     thirty_five.right = fifty
 
     return BinaryTree(four)
+
+@pytest.fixture()
+def tree3():
+    one = Node(1)
+    two = Node(2)
+    three = Node(3)
+    four = Node(4)
+    seven = Node(7)
+
+    one.left = two
+    one.right = three
+    three.left = four
+    three.right = seven    
+
+    return BinaryTree(one)
 
 def test_exists():
     assert tree_intersection
@@ -68,3 +81,6 @@ def test_empty_tree():
 
 def test_intersection1(tree1, tree2):
     assert tree_intersection(tree1, tree2) == [10, 12, 16, 17, 20, 35, 50]
+
+def test_intersection2(tree1, tree3):
+    assert tree_intersection(tree1, tree3) == [7]
--- code-challenges/tree_intersection/tree_intersection.py
+++ code-challenges/tree_intersection/tree_intersection.py
@@ -5,7 +5,6 @@ def tree_intersection(tree1, tree2):
         return []
         
     tree1_values = tree1.in_order()
-    print('tree1_values:', tree1_values)
 
     intersection = []
 
@@ -17,30 +16,10 @@ def tree_intersection(tree1, tree2):
             _visit(node.left)
     
         if node.value in tree1_values:
-            intersection.append(node)
+            intersection.append(node.value)
 
         if node.right:
             _visit(node.right)
 
     _visit(tree2.root)
-    print('intersection:', intersection)
-
     return intersection 
-
-
-# from tree import BinaryTree, Node
-
-# def tree_intersection(tree1, tree2):
-#     if tree1.root == None or tree2.root == None: 
-#         return []
-        
-#     tree1_values = tree1.in_order()
-#     tree2_values = tree2.in_order()
-
-#     intersection = []
-
-#     for i in tree2_values:
-#         if i in tree1_values:
-#             intersection.append(i)
-
-#     return intersection 
